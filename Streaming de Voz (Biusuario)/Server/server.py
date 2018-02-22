80743c22a7d82786aecddd5089ab81ab86ad105c
Fercho <fercho@ferchopc>
2018-02-21 20:51:27 -0500

    Mejorando Streaming

--- Streaming de Voz (Biusuario)/Server/server.py
+++ Streaming de Voz (Biusuario)/Server/server.py
@@ -2,6 +2,8 @@ import zmq
 import time
 import sys
 import os
+import threading
+
 
 def main():
 	if len(sys.argv) != 2:
@@ -17,17 +19,30 @@ def main():
 
 	listausuarios=[]
 
+
 	while True:
 
 		msg= s.recv_json()
-		if msg["op"]=="Registrar":
+		if msg["op"]=="Conectarse":
 			peta+=1
+			
+			#Añadiendo cada usuario que se conecta a la lista
 			listausuarios.append({"nombre": msg["nombreenv"], "ip": msg["ip"],"port":peta})
-			for usuario in listausuarios:
-				print (usuario["nombre"])
+			'''for usuario in listausuarios:
+				print (usuario["nombre"])'''
+
 			s.send_json(peta)
-		elif msg["op"]=="Enviar":
-			print("")
+
+			usertoconect=s.recv_json()
+
+			if usertoconect in listausuarios["nombre"]:
+
+				conexion = context.socket(zmq.REP)
+				conexion.connect("tcp://{}:{}".format(listausuarios["ip"],listausuarios["port"]))
+
+
+		else:
+			print("Operación no definida!! -_-")
 
 if __name__=='__main__':
     main()
--- Streaming de Voz (Biusuario)/client.py
+++ Streaming de Voz (Biusuario)/client.py
@@ -6,7 +6,9 @@ import os
 import os.path as path
 import socket
 from random import randrange
-import pygame
+import pip
+#import pyaudio
+import wave
 
 def main():
     sip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
@@ -14,7 +16,6 @@ def main():
     ipuse=sip.getsockname()
     ip=ipuse[0]
     print(ip)
-    pygame.init()
 
     if len(sys.argv)!=3:
         print ("Error!!!")
@@ -23,22 +24,60 @@ def main():
     ip = sys.argv[1] #Server's ip
     port = sys.argv[2] #Server's port
 
-    context= zmq.Context()
+    context= zmq.Context() #Contexto para los sockets
+
     sc = context.socket(zmq.REQ)
     sc.connect("tcp://{}:{}".format(ip,port))
+    #Nobre de usuario que se está conectando
     name=input("Nombre")
-    sc.send_json({"op":"Registrar","nombreenv": name,"ip":ip})
-    Resp = sc.recv_json()
-    print(Resp)
-    print("ciclo")
+    sc.send_json({"op":"Conectarse","nombreenv": name,"ip":ip})
+    puerto = sc.recv_json()
 
+    com=input("Digite el nombre del usuario con el cual desea conectarse")
+    sc.send_json(com)
+
+
+    canal= context.socket(zmq.REP)
+    canal.bind("tcp://*:{}".format(puerto))
+
+    #Ciclo para el Streaming
     while True:
-        print("fgf")
-        for event in pygame.event.get():
-            if event.type is KEYDOWN:
-                print("holaaaa")
-            else:
-                print("pald")   
+          
+        CHUNK = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 44100
+        RECORD_SECONDS = 0.55
+        WAVE_OUTPUT_FILENAME = "voice.wav"
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        frames_per_buffer=CHUNK)
+
+        print("* recording")
+
+        frames = []
+
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
+
+        print("* done recording")
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
 
 
     pygame.quit()
