0823f3322c3d579934425178d005b318d444076c
FerchoG <ferchogir@gmail.com>
2018-03-02 23:32:35 -0500

    Finish streaming biusuario...

--- Streaming de Voz (Biusuario)/client.py
+++ Streaming de Voz (Biusuario)/client.py
@@ -7,80 +7,65 @@ import socket
 import pyaudio
 import wave
 import threading
-
-
-
-CHUNK = 1024
+import time
+#variables
+CHUNK = 512
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
-
-def Enviar(CanalSolicitudes, Receptor):
-
-	p = pyaudio.PyAudio()
-
-
-	stream = p.open(format=FORMAT,
-	channels=CHANNELS,
-	rate=RATE,
-	input=True,
-	frames_per_buffer=CHUNK)
-
-
-	#Ciclo para grabar audio y enviarlo
-	while True:
-
-		audio = stream.read(CHUNK)
-
-		CanalSolicitudes.send_json({"op": "Online","touser": Receptor, "audio": audio.decode('UTF-8','ignore')})
-		CanalSolicitudes.recv_string()
-
-	stream.stop_stream()
-	stream.close()
-	p.terminate()
 
 
 
 def Recibir(CanalServidor, CanalMio):
 
 	p = pyaudio.PyAudio()
-
-
 	stream = p.open(format=FORMAT,
-	channels=CHANNELS,
-	rate=RATE,
-	input=True,
-	frames_per_buffer=CHUNK)
-
+					channels=CHANNELS,
+					rate=RATE,
+					output=True,
+					frames_per_buffer=CHUNK
+					)
 	while True:
 
 		solicitud = CanalMio.recv_json()
-
-		if solicitud["op"]=="Llamando":
-			print("El usuario {} te está llamando".format(solicitud["solicitud"]))
-			print("Llamando->Online")
-			CanalMio.send_string("Listo")
-
-		elif solicitud["op"] ==  "Estableciendo":
-
+		if solicitud["op"] ==  "Estableciendo":
 			#Hilo para enviar info al servidor
+			CanalMio.send_json("Listo")
 			threading.Thread(target= Enviar, args=(CanalServidor, solicitud["receptor"])).start()
-			CanalMio.send_string("Listo")
-
-
+			
 		elif solicitud["op"] == "Online":
-			stream.write(solicitud["audio"].encode('UTF-8','ignore'))
-			CanalMio.send_string("Listo")
 
-		else:
+			print("escucho")
+			stream.write(solicitud["audio"].encode('UTF-16','ignore'))
 			CanalMio.send_string("Listo")
 					
-
 	stream.stop_stream()
 	stream.close()
 	p.terminate()
 
+def Enviar(CanalServidor, Receptor):
 
+	p = pyaudio.PyAudio()
+
+
+	stream = p.open(format=FORMAT,
+					channels=CHANNELS,
+					rate=RATE,
+					input=True,
+					frames_per_buffer=CHUNK	
+					)
+
+	#Ciclo para grabar audio y enviarlo
+	while True:
+
+		audio = stream.read(CHUNK)
+		print("hablo")
+		CanalServidor.send_json({"op": "Online","touser": Receptor, "audio": audio.decode('UTF-16','ignore')})
+		CanalServidor.recv_json()
+
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
 
 
 
@@ -101,9 +86,7 @@ def main():
 	    exit()
 
 
-
 	context= zmq.Context() #Contexto para los sockets
-
 	#Conexión con el servidor
 	sc = context.socket(zmq.REQ)
 	sc.connect("tcp://{}:{}".format(ip,port))
@@ -114,14 +97,20 @@ def main():
 	puerto = sc.recv_json()
 
 	eleccion = input("¿Desea realizar una llamada? \n 1.Si\n 2.No \n Tu Respuesta>> ")
+	
 
-
-	if eleccion=='1':
-		com=input("Digite el nombre del usuario con el cual desea conectarse: ")
-		sc.send_json({"op":"Llamar","nombreenv": name})
-		sc.recv_json()
-		sc.send_json(com)
-
+	if eleccion == '1':
+		eleccion2 = input("\n \n ¿Con quien desea conectarse? \n 1.Persona especifica\n 2.Grupo conectado al servidor \n Tu Respuesta>> ")
+		if eleccion2=='1':
+			com=input("Digite el nombre del usuario con el cual desea conectarse: ")
+			sc.send_json({"op":"Llamar","nombreenv": name})
+			sc.recv_json()
+			sc.send_json(com)
+			sc.recv_json()
+		if eleccion2 == '2':
+			sc.send_json({"op":"LlamarGrupo","nombreenv": name})
+			sc.recv_json()
+ 
 	#Socket para escuchar
 	canal= context.socket(zmq.REP)
 	canal.bind("tcp://*:{}".format(puerto))
--- Streaming de Voz (Biusuario)/server.py
+++ Streaming de Voz (Biusuario)/server.py
@@ -16,63 +16,62 @@ def main():
 	s = context.socket(zmq.REP)
 	s.bind("tcp://*:{}".format(port))
 
-	#Asignando puerto
+	#Asignando puerto a clientes de entrada
 	peta=4000
 
 	listausuarios={}
 
 
 	while True:
-
-		msg= s.recv_json()
-
+		msg = s.recv_json()
 		if msg["op"]=="Registrarse":
 			peta+=1
-			
 			#Añadiendo cada usuario que se conecta al diccionario
 			usuarionuevo = context.socket(zmq.REQ)
 			usuarionuevo.connect("tcp://{}:{}".format(msg["ip"], peta))
 			listausuarios[msg["nombreenv"]] = usuarionuevo
-			
-			#Enviandole el puerto al usuario
+			#Enviandole el puerto al usuario para crear el canal.
 			s.send_json(peta)
 
-
-		elif msg["op"]=="Llamar":
+		elif msg["op"]=="LlamarGrupo":
 
 			s.send_json("Listo")
+			for usertoconect in listausuarios:
+				if usertoconect != msg["nombreenv"]:
+					emisor = listausuarios[msg["nombreenv"]]
+					emisor.send_json({"op": "Estableciendo", "receptor": usertoconect})
+					emisor.recv_string()
+					receptor = listausuarios[usertoconect]
+					receptor.send_json({"op":"Estableciendo","receptor": msg["nombreenv"]})
+					receptor.recv_string()
+
+		elif msg["op"]=="Llamar":
+			s.send_json("Listo")
 			usertoconect=s.recv_json()
+			s.send_json("Listo")
 
 			if listausuarios.get(usertoconect)!= None:				
-
-				receptor = listausuarios[usertoconect]
-				receptor.send_json({"op":"Llamando", "solicitud": msg["nombreenv"]})
-				receptor.recv_string()
-
 
 				emisor = listausuarios[msg["nombreenv"]]
 				emisor.send_json({"op": "Estableciendo", "receptor": usertoconect})
 				emisor.recv_string()
-
-
+				receptor = listausuarios[usertoconect]
 				receptor.send_json({"op":"Estableciendo","receptor": msg["nombreenv"]})
 				receptor.recv_string()
-
 			else:
 				print("Usuario no conectado")
 				
 				
-		elif msg["op"] == "Online":
+		elif msg["op"] == "Online":			
+				s.send_json("Listo")
 				receptor=listausuarios[msg["touser"]]
-				s.send_string("Listo")
 				receptor.send_json(msg)
 				receptor.recv_string()
-
-			
+				print("conectando")
 
 
 		else:
-			print("Operación no definida!! -_-")
+			print("Operacion no permitida")
 
 if __name__=='__main__':
     main()
