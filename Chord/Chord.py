728969cf3d4753ab628741ee007229bb8ae3525c
JhonAlbert <jhonb93@gmail.com>
2018-05-29 19:49:05 -0500

    Mejorando...

--- Chord/Chord.py
+++ Chord/Chord.py
@@ -9,9 +9,9 @@ from time import sleep
 
 
 
-cant_nodos = 16
+cant_nodos = 256
 
-pot = int(math.log2(cant_nodos))
+pot = int(math.log2(cant_nodos)) 
 
 
 class Nodo():
@@ -70,17 +70,26 @@ class Nodo():
 	#Pendiente de Modificiacion
 	def Actualizar_Finger(self,table):
 		for key in self.finger_table:
-			finger_table[key] = table[key]
+			self.finger_table[key] = table[key]
+
+
+	#Imprimir finger
+
+	def Mostrar_Finger(self):
+		for key in self.finger_table:
+			print(key)
+			print(self.finger_table[key])
+			print("\n")
 
 #Funcion para verificar si estoy en el rango del nodo
 def Verificar(id_entrada, mi_x, mi_y):
 	resultado = False
 	if( mi_x > mi_y):
-		if(id_entrada > mi_x or id_entrada < mi_y):
+		if(id_entrada >= mi_x or id_entrada <= mi_y):
 			resultado = True
 
 	else:
-		if(id_entrada > mi_x and id_entrada < mi_y):
+		if(id_entrada >= mi_x and id_entrada <= mi_y):
 			resultado = True
 
 	return resultado
@@ -88,13 +97,14 @@ def Verificar(id_entrada, mi_x, mi_y):
 
 
 
-def Server(canal_servidor, port, mi_nodo):
+def Server(canal_servidor, port, mi_nodo,contexto):
 	canal_servidor.bind("tcp://*:"+port)
 
 	while True:
 		mensaje = canal_servidor.recv_json()
 
 		if (mensaje["op"] == "conexion"):
+			print("Albert")
 			entrada_nodo_id = mensaje["id"]
 
 			aqui_es = Verificar(entrada_nodo_id, mi_nodo.GetX(), mi_nodo.GetY())
@@ -102,65 +112,75 @@ def Server(canal_servidor, port, mi_nodo):
 			if(aqui_es):
 				if(mi_nodo.GetId() > entrada_nodo_id):
 
-					data={"op": "si", "x":mi_nodo.GetX() , "y":entrada_nodo_id,"server_id": mi_nodo.GetId()}
+					data={"op": "si", "x":mi_nodo.GetX() , "y":entrada_nodo_id}
 					mi_nodo.SetX(entrada_nodo_id+1)
 					mi_nodo.SetY(mi_nodo.GetId())
 					print(mi_nodo.GetX())
 					print(mi_nodo.GetY())
-					canal_servidor.send_json(data)
+					
 
 				else:
 
-					data={"op": "si", "x":mi_nodo.GetY()+1, "y": entrada_nodo_id, "server_id": mi_nodo.GetId()}
+					data={"op": "si", "x":mi_nodo.GetY()+1, "y": entrada_nodo_id}
 					mi_nodo.SetX(entrada_nodo_id+1)
 					mi_nodo.SetY(mi_nodo.GetId())
 					print(mi_nodo.GetX())
 					print(mi_nodo.GetY())
-					canal_servidor.send_json(data)
 			else:
 				table = mi_nodo.GetFinger()
-				minimo = 70
-				id_min = 0
-				ip_min = 0
-				puerto_min = 0
+				Stop = True
+				
 				for llave in table:
-					if(table[llave]["id"] - entrada_nodo_id < minimo):
-						id_min = table[llave]["id"]
-						ip_min = table[llave]["ip"]
-						puerto_min = table[llave]["puerto"]
-						minimo = table[llave]["id"] - entrada_nodo_id
-				data={"op" : "siguiente", "id" : id_min, "ip": ip_min, "puerto": puerto_min}
-				canal_servidor.send_json(data)
+					if(table[llave]["id"] > entrada_nodo_id):
+						if(Stop):
+							id_Sig = table[llave]["id"]
+							ip_Sig = table[llave]["ip"]
+							puerto_Sig = table[llave]["puerto"]
+							Stop=False
+
+				data={"op" : "siguiente", "id" : id_Sig, "ip": ip_Sig, "puerto": puerto_Sig}
+			canal_servidor.send_json(data)	
 
 		elif(mensaje["op"] == "actualizando"):
+			mi_nodo.Mostrar_Finger()
 			llave_check = mensaje["llave"]
+
+			print(llave_check)
 			if(Verificar(llave_check, mi_nodo.GetX(), mi_nodo.GetY())):
+				print("gholalalal")
 				msj = {"op": "es_llave", "id": mi_nodo.GetId(), "ip": mi_nodo.GetIp() , "puerto": mi_nodo.GetPuerto()}
 			else:
 				my_finger = mi_nodo.GetFinger()
-				minimo = 70
+				Stop = True
 				for key in  my_finger:
-					if( my_finger[key]["id"] - llave_check < minimo):
-						minimo = my_finger[key]["id"] - llave_check
-						sgte_id = my_finger[key]["id"]
-						sgte_ip = my_finger[key]["ip"]
-						sgte_port = my_finger[key]["puerto"]
+					if( my_finger[key]["id"] > llave_check):
+						if(Stop):
+							sgte_id = my_finger[key]["id"]
+							sgte_ip = my_finger[key]["ip"]
+							sgte_port = my_finger[key]["puerto"]
+							Stop=False
+
 				msj = {"op": "no_es_llave", "id": sgte_id, "ip": sgte_ip , "puerto": sgte_port}
 			canal_servidor.send_json(msj)
 
 		elif(mensaje["op"] == "rueda_la_bola"):
-			if(mensaje["start"] != mi_nodo.GetId()):
-				#Actualizando Finger
-				finger = mi_nodo.GetFinger()
-				
-				for key in finger:
-					if(Verificar(key, mensaje["x"], mensaje["y"])):
-						finger[key]["id"] = mensaje["id"]
-						finger[key]["ip"] = mensaje["ip"]
-						finger[key]["puerto"] = mensaje["puerto"]
-				canal_servidor.send_string("Listo")
+			
+			print("-------------------------------------------------")
+			#Actualizando Finger
+			finger = mi_nodo.GetFinger()
+			
+			for key in finger:
+				if(Verificar(key, mensaje["x"], mensaje["y"])):
+					finger[key]["id"] = mensaje["id"]
+					finger[key]["ip"] = mensaje["ip"]
+					finger[key]["puerto"] = mensaje["puerto"]
+			canal_servidor.send_string("Listo")
 
-				socket_sucesor = context.socket(zmq.REQ)
+			mi_nodo.Actualizar_Finger(finger)
+			mi_nodo.Mostrar_Finger()			
+
+			if(mensaje["start"] != finger[mi_nodo.GetId() + 2 ** 0]["id"]):
+				socket_sucesor = contexto.socket(zmq.REQ)
 				key_sucesor = mi_nodo.GetId() + 2**0 % cant_nodos
 				ip_sucesor = finger[key_sucesor]["ip"]
 				puerto_sucesor = finger[key_sucesor]["puerto"]
@@ -168,8 +188,9 @@ def Server(canal_servidor, port, mi_nodo):
 				solicitud = {"op": "rueda_la_bola" , "id": mi_nodo.GetId(), "x": mi_nodo.GetX(), "y": mi_nodo.GetY(), "ip": mi_nodo.GetIp(), "puerto": mi_nodo.GetPuerto(), "start": mensaje["start"]}
 				socket_sucesor.connect(dir_sucesor)
 				socket_sucesor.send_json(solicitud)
-			else:
-				canal_servidor.send_string("termino_actualizar")
+				socket_sucesor.disconnect(dir_sucesor)
+			
+
 
 
 
@@ -178,7 +199,8 @@ def main():
 		my_ip = sys.argv[1]
 		my_port = sys.argv[2]
 
-		ide = random.randrange(0,cant_nodos-1)
+		#ide = random.randrange(0,cant_nodos-1)
+		ide=int(input("Id : "))
 		print(ide)
 		print("\n")
 
@@ -195,7 +217,9 @@ def main():
 	if(len(sys.argv) == 5):
 		my_ip = sys.argv[1]
 		my_port = sys.argv[2]
-		ide = random.randrange(0,cant_nodos-1)
+		#ide = random.randrange(0,cant_nodos-1)
+		ide=int(input("Id : "))
+
 		print(ide)
 		print("\n")
 
@@ -212,19 +236,21 @@ def main():
 	context= zmq.Context()
 	socket_cliente = context.socket(zmq.REQ)
 	socket_servidor = context.socket(zmq.REP)
-	thread_server = threading.Thread(target=Server, args=(socket_servidor, my_port, nuevo))
+	thread_server = threading.Thread(target=Server, args=(socket_servidor, my_port, nuevo, context))
 	thread_server.start()
 
 	#Ciclo para conectar nodo
+	
 	while not conectado:
 
 		socket_cliente.connect(address)
 
-		data = {"op" : "conexion","id" : nuevo.GetId(), "ip" : nuevo.GetIp(), "port" : nuevo.GetPuerto()}
+		data = {"op" : "conexion","id" : nuevo.GetId(), "ip" : nuevo.GetIp(), "puerto" : nuevo.GetPuerto()}
 		socket_cliente.send_json(data)
 		respuesta = socket_cliente.recv_json()
 
 		if(respuesta["op"] == "si"):
+			print("Dubel")
 			print(respuesta["op"])
 			nuevo.SetX(respuesta["x"])
 			nuevo.SetY(respuesta["y"])
@@ -233,24 +259,34 @@ def main():
 
 			#Actualizando Finger
 			new_finger = {}
-			
-			for i in range(0,pot):
+			print("Puto")
+			for i in range(0,pot-1):
 				encontrado = False
 				llave = (nuevo.GetId() + 2 ** i) % cant_nodos
-				while not encontrado:
-					socket_cliente.send_json({"op": "actualizando", "llave": llave})
-					mensaje = socket_cliente.recv_json()
-					if (mensaje["op"] == "es_llave"):
-						new_finger[llave] = {"id" : mensaje["id"], "ip": mensaje["ip"] , "puerto" : mensaje["puerto"]}
-						encontrado=True
-					elif (mensaje["op"] == "no_es_llave"):
-						sgte_ip = mensaje["ip"]
-						sgte_port = mensaje["puerto"]
-						socket_cliente.disconnect(address)
-						address = "tcp://"+sgte_ip+":"+sgte_port
-						socket_cliente.connect(address)
+				print(llave)
+				
+				if(Verificar(llave,nuevo.GetX(),nuevo.GetY())):
+					new_finger[llave] = {"id" : nuevo.GetId(), "ip": nuevo.GetIp() , "puerto" : nuevo.GetPuerto()}
+					print("hola")
+				else:
+					while not encontrado:
+						socket_cliente.send_json({"op": "actualizando", "llave": llave})
+						#print(llave)
+						mensaje = socket_cliente.recv_json()
+						#print(mensaje)
+						if (mensaje["op"] == "es_llave"):
+							new_finger[llave] = {"id" : mensaje["id"], "ip": mensaje["ip"] , "puerto" : mensaje["puerto"]}
+							encontrado=True
+						elif (mensaje["op"] == "no_es_llave"):
+							sgte_ip = mensaje["ip"]
+							sgte_port = mensaje["puerto"]
+							socket_cliente.disconnect(address)
+							address = "tcp://"+sgte_ip+":"+sgte_port
+							socket_cliente.connect(address)
 			nuevo.Actualizar_Finger(new_finger)
+			
 			print("Actualizado con exito")
+			nuevo.Mostrar_Finger()
 			conectado=True
 
 		elif(respuesta["op"] == "siguiente"):
@@ -260,19 +296,20 @@ def main():
 			socket_cliente.disconnect(address)
 			address = "tcp://"+sgte_ip+":"+sgte_port
 
+
 	
 
+		if(conectado):
+			sucesor_finger = nuevo.GetFinger()
+			key_sucesor = nuevo.GetId() + 2 ** 0
+			sucesor={"id": sucesor_finger[key_sucesor]["id"], "ip": sucesor_finger[key_sucesor]["ip"], "puerto": sucesor_finger[key_sucesor]["puerto"]}
+			solicitud = {"op": "rueda_la_bola" , "id": nuevo.GetId(), "x": nuevo.GetX(), "y": nuevo.GetY(), "ip": nuevo.GetIp(), "puerto": nuevo.GetPuerto(), "start": nuevo.GetId()}
 
-		sucesor_finger = nuevo.GetFinger()
-		key_sucesor = nuevo.GetId() + 2 ** 0
-		sucesor={"id": sucesor_finger[key_sucesor]["id"], "ip": sucesor_finger[key_sucesor]["ip"], "puerto": sucesor_finger[key_sucesor]["puerto"]}
-		solicitud = {"op": "rueda_la_bola" , "id": nuevo.GetId(), "x": nuevo.GetX(), "y": nuevo.GetY(), "ip": nuevo.GetIp(), "puerto": nuevo.GetPuerto(), "start": nuevo.GetId()}
-
-		socket_cliente.disconnect(address)
-		address = "tcp://"+sucesor["ip"]+":"+sucesor["puerto"]
-		socket_cliente.connect(address)
-		socket_cliente.send_json(solicitud)
-		socket_cliente.recv_string()
+			socket_cliente.disconnect(address)
+			address = "tcp://"+sucesor["ip"]+":"+sucesor["puerto"]
+			socket_cliente.connect(address)
+			socket_cliente.send_json(solicitud)
+			socket_cliente.recv_string()
 
 
 main()
