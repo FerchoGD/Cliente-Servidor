a614db3c03857baeead03fdd0bdc14009130304d
JhonAlbert <jhonb93@gmail.com>
2018-06-01 23:33:38 -0500

    Chord1.0

--- Chord/prueba2.py
+++ Chord/prueba2.py
@@ -7,8 +7,13 @@ import os
 import math
 from time import sleep
 
-cant_nodos = 64
+
+
+cant_nodos = 16
+
 pot = int(math.log2(cant_nodos)) 
+
+
 class Nodo():
 
 	def __init__(self,ip,puerto,ide):
@@ -58,7 +63,8 @@ class Nodo():
 			self.finger_table[key] = table[key]
 
 
-	#Imprimir finger Table
+	#Imprimir finger
+
 	def Mostrar_Finger(self):
 		for key in self.finger_table:
 			print(str(key) +" "+str(self.finger_table[key]))
@@ -77,83 +83,143 @@ def Verificar(id_entrada, mi_x, mi_y):
 
 	return resultado
 
-#Funcion que permite recorrer la finger_table de un nodo y pasarnos los parametros de su sucesor ideal.
-def encontrarNodo(table,entrada_nodo_id,op):
+	
+def encontrar(table,entrada_nodo_id,idn):
+	banderaKeyFinal= -1
+	
 	for llave in table:
-		if(Verificar(entrada_nodo_id,table[llave]["rangollave"]["x"],table[llave]["rangollave"]["y"])):
+		print(llave)
+		print(idn)
+		print(table[llave]["id"])
+
+
+		if(table[llave]["id"] == idn):
+			sgte_id = table[KeyFinal]["id"]
+			sgte_ip = table[KeyFinal]["ip"]
+			sgte_port = table[KeyFinal]["puerto"]
+			data={"op" : "siguiente", "id" : sgte_id, "ip": sgte_ip, "puerto": sgte_port}
+			return data
+
+		if(banderaKeyFinal > table[llave]["id"]):
 			sgte_id = table[llave]["id"]
 			sgte_ip = table[llave]["ip"]
 			sgte_port = table[llave]["puerto"]
-			if(op==1):
-				data={"op" : "siguiente", "id" : sgte_id, "ip": sgte_ip, "puerto": sgte_port}
-			else:
-				data={"op" : "no_es_llave", "id" : sgte_id, "ip": sgte_ip, "puerto": sgte_port}
+			data={"op" : "siguiente", "id" : sgte_id, "ip": sgte_ip, "puerto": sgte_port}
 			return data
-
+			
+		if(table[llave]["id"] > entrada_nodo_id):
+			print("Noooo")
+			sgte_id = table[llave]["id"]
+			sgte_ip = table[llave]["ip"]
+			sgte_port = table[llave]["puerto"]
+			data={"op" : "siguiente", "id" : sgte_id, "ip": sgte_ip, "puerto": sgte_port}
+			return data
+					
+		banderaKeyFinal=table[llave]["id"]
 		KeyFinal=llave
-	#print("No estoy en la finger")
+
+
+	print("No estoy ")
 	sgte_id = table[KeyFinal]["id"]
 	sgte_ip = table[KeyFinal]["ip"]
 	sgte_port = table[KeyFinal]["puerto"]
-	#print(str(sgte_id)+"  "+str(sgte_ip)+" "+str(sgte_port))
-	if(op==1):
-		data={"op" : "siguiente", "id" : sgte_id, "ip": sgte_ip, "puerto": sgte_port}
-	else:
-		data={"op" : "no_es_llave", "id" : sgte_id, "ip": sgte_ip, "puerto": sgte_port}
+	print(str(sgte_id)+"  "+str(sgte_ip)+" "+str(sgte_port))
+	data={"op" : "siguiente", "id" : sgte_id, "ip": sgte_ip, "puerto": sgte_port}
 	return data
 
 
-#Funcion que se ejecuta en el hilo que queda esperando el ingreso de un mensaje
+
 def Server(canal_servidor, port, mi_nodo,contexto):
 	canal_servidor.bind("tcp://*:"+port)
 
 	while True:
 		mensaje = canal_servidor.recv_json()
-		#condicional por medio del cual el nodo entrante busca su puesto en el chord
+
 		if (mensaje["op"] == "conexion"):
 			print("\n")
-			print("Se esta conectado a mi el nodo "+str(mensaje["id"]))
+			print("Se estan conectado a mi"+"\n")
 			entrada_nodo_id = mensaje["id"]
+
 			aqui_es = Verificar(entrada_nodo_id, mi_nodo.GetX(), mi_nodo.GetY())
-			#Si el nodo entrante esta en el rango de llaves del nodo de ingreso.
+
 			if(aqui_es):
+				# if(mi_nodo.GetId() > entrada_nodo_id):
+
 				data={"op": "si", "x":mi_nodo.GetX() , "y":entrada_nodo_id}
 				mi_nodo.SetX((entrada_nodo_id+1) % cant_nodos)
 				mi_nodo.SetY((mi_nodo.GetId())% cant_nodos)
-				print("Rango: "+str(mi_nodo.GetX()) +" - "+ str(mi_nodo.GetY()))	
+				print("Rango: "+str(mi_nodo.GetX()) +" - "+ str(mi_nodo.GetY()))
+					
 			else:
-				print("Lo siento, te comunico con un nodo sucesor.")
 				table = mi_nodo.GetFinger()
-				data=encontrarNodo(table,entrada_nodo_id,1)
+				idn=mi_nodo.GetId()
+				data=encontrar(table,entrada_nodo_id,idn)
 			canal_servidor.send_json(data)	
-		#Condicional que nos permite actualizar la finger table del nodo que esta ingresando.
+
 		elif(mensaje["op"] == "actualizando"):
-			#print("Actualizando Inicio  --- Actualizando finger del nuevo")
+			print("Actualizando Inicio  --- Actualizando finger del nuevo")
 			#mi_nodo.Mostrar_Finger()
 			llave_check = mensaje["llave"]
-			#print(llave_check)
+
+			print(llave_check)
 			if(Verificar(llave_check, mi_nodo.GetX(), mi_nodo.GetY())):
-				#print("SI estoy")
 				msj = {"op": "es_llave", "id": mi_nodo.GetId(), "ip": mi_nodo.GetIp() , "puerto": mi_nodo.GetPuerto(), "rx" :mi_nodo.GetX(), "ry" : mi_nodo.GetY()}
 			else:
 				my_finger = mi_nodo.GetFinger()
-				msj=encontrarNodo(my_finger,llave_check,2)
+				Stop1 = True
+				Stop2 = True
+				banderaKeyFinal=-1
+				for key in  my_finger:
+					if(Stop2):
+						if(banderaKeyFinal > my_finger[key]["id"]):
+							sgte_id = my_finger[key]["id"]
+							sgte_ip = my_finger[key]["ip"]
+							sgte_port = my_finger[key]["puerto"]
+							Stop1=False
+							Stop2 = False
+					if(Stop2):
+						if( my_finger[key]["id"] >= llave_check):
+							if(Stop1):
+								sgte_id = my_finger[key]["id"]
+								sgte_ip = my_finger[key]["ip"]
+								sgte_port = my_finger[key]["puerto"]
+								Stop1=False
+								Stop2 = False
+					banderaKeyFinal=my_finger[key]["id"]
+					KeyFinal=key
+				if(Stop1):
+					print("No estoy ")
+					sgte_id = my_finger[KeyFinal]["id"]
+					sgte_ip = my_finger[KeyFinal]["ip"]
+					sgte_port = my_finger[KeyFinal]["puerto"]
+					print(str(sgte_id)+"  "+str(sgte_ip)+" "+str(sgte_port))
+
+
+
+				msj = {"op": "no_es_llave", "id": sgte_id, "ip": sgte_ip , "puerto": sgte_port}
 			canal_servidor.send_json(msj)
-			#print("Actualizando Fin")
-		#Condicional que ejecuta la orden de actualizacion de las finger tables.
+			print("Actualizando Fin")
+
+
 		elif(mensaje["op"] == "rueda_la_bola"):
 			
 			print("-------------------------------------------------")
 			#Actualizando Finger
 			finger = mi_nodo.GetFinger()
+			
 			for key in finger:
 				if(Verificar(key, mensaje["rx"], mensaje["ry"])):
 					finger[key]["id"] = mensaje["id"]
 					finger[key]["ip"] = mensaje["ip"]
 					finger[key]["puerto"] = mensaje["puerto"]
 					finger[key]["rangollave"]={"x": mensaje["rx"],"y":mensaje["ry"]}
+				
+				print(key)
+				print(mensaje["rxi"])
+				print(mensaje["ryi"])
 
 				if(Verificar(key, mensaje["rxi"], mensaje["ryi"])):
+					print("Siiiiiiiiii")
 					finger[key]["rangollave"]={"x": mensaje["rxi"],"y":mensaje["ryi"]}
 
 			canal_servidor.send_string("Listo")
@@ -172,14 +238,20 @@ def Server(canal_servidor, port, mi_nodo,contexto):
 				socket_sucesor.send_json(solicitud)
 				socket_sucesor.disconnect(dir_sucesor)
 			
+
+
+
+
 def main():
-	#Solo para el ingreso del primer nodo del chord.
 	if(len(sys.argv) == 3):
 		my_ip = sys.argv[1]
 		my_port = sys.argv[2]
-		ide = random.randrange(0,cant_nodos-1)
-		#ide=int(input("Id : "))
+
+		#ide = random.randrange(0,cant_nodos-1)
+		ide=int(input("Id : "))
 		print(ide)
+		print("\n")
+
 		nuevo = Nodo(my_ip, my_port,ide)
 		comp_x = ide + 1
 		comp_y = ide
@@ -187,15 +259,18 @@ def main():
 		nuevo.SetX(comp_x)
 		nuevo.SetY(comp_y)
 		nuevo.Finger()
+
 		conectado = True
-	#Se ejecuta del segundo nodo en adelante
+
 	if(len(sys.argv) == 5):
 		my_ip = sys.argv[1]
 		my_port = sys.argv[2]
-		ide = random.randrange(0,cant_nodos-1)
-		#ide=int(input("Id : "))
+		#ide = random.randrange(0,cant_nodos-1)
+		ide=int(input("Id : "))
+
 		print(ide)
 		print("\n")
+
 		nuevo = Nodo(my_ip, my_port,ide)
 		nuevo.Finger()
 
@@ -205,7 +280,7 @@ def main():
 		address = "tcp://"+other_ip+":"+other_port
 		conectado = False
 
-	#Se crea el contexto y se ejecuta el hilo de escucha.
+
 	context= zmq.Context()
 	socket_cliente = context.socket(zmq.REQ)
 	socket_servidor = context.socket(zmq.REP)
@@ -213,6 +288,7 @@ def main():
 	thread_server.start()
 
 	#Ciclo para conectar nodo
+	
 	while not conectado:
 
 		socket_cliente.connect(address)
@@ -221,24 +297,25 @@ def main():
 		socket_cliente.send_json(data)
 		respuesta = socket_cliente.recv_json()
 
-		#Condicional ejecutada despues de saber donde se debe conectar el nodo.
 		if(respuesta["op"] == "si"):
 			print(respuesta["op"])
 			nuevo.SetX(respuesta["x"])
 			nuevo.SetY(respuesta["y"])
-			print("Rango: "+str(nuevo.GetX()) +" -- "+ str(nuevo.GetY())+"\n")
+			print("Rango: "+str(nuevo.GetX()) +" - "+ str(nuevo.GetY())+"\n"+"\n")
+
+			
+
 			#Actualizando Finger
 			new_finger = {}
 			for i in range(0,pot):
 				encontrado = False
 				llave = (nuevo.GetId() + 2 ** i) % cant_nodos
-				#print(llave)
+				print(llave)
 				
 				if(Verificar(llave,nuevo.GetX(),nuevo.GetY())):
 					new_finger[llave] = {"id" : nuevo.GetId(), "ip": nuevo.GetIp() , "puerto" : nuevo.GetPuerto(), "rangollave" : {"x" :nuevo.GetX(), "y" : nuevo.GetY()}}
-					#print("Me pertenece esta llave")
+					print("Me pertenece esta llave")
 				else:
-					#Llenado de finger table
 					while not encontrado:
 						socket_cliente.send_json({"op": "actualizando", "llave": llave})
 						#print(llave)
@@ -259,7 +336,6 @@ def main():
 			nuevo.Mostrar_Finger()
 			conectado=True
 
-		#Condicion que se ejecuta cuando se necesita conectar al nodo siguiente de una finger_table de un nodo conocido
 		elif(respuesta["op"] == "siguiente"):
 			sgte_id =  respuesta["id"]
 			sgte_ip = respuesta["ip"]
@@ -267,7 +343,9 @@ def main():
 			socket_cliente.disconnect(address)
 			address = "tcp://"+sgte_ip+":"+sgte_port
 
-		#Si el nodo se encuentra conectado, y su fingerTable actualizada 
+
+	
+
 		if(conectado):
 			sucesor_finger = nuevo.GetFinger()
 			key_sucesor = (nuevo.GetId() + 2 ** 0) % cant_nodos
